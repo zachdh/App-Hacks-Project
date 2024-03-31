ae85262a91f87c90b92d6e92a41e95b4a809fce8
Zachary Huang <142925256+zachdh@users.noreply.github.com>
2024-03-30 22:38:37 -0700

    Add movement rework
    Allows for one movement feature. All movement is bound to the space bar.

--- player.gd
+++ player.gd
@@ -2,30 +2,39 @@ extends CharacterBody2D
 
 const GRAVITY : int = 4200
 
-
-
-var starting_x
-var starting_y
 var direction : int = 1
-
+var jumps : int = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
-	$AnimatedSprite2D.play()
-	starting_x = self.position.x
-	starting_y = self.position.y
+	#add idle animation
+	#$AnimatedSprite2D.play("idle")
+	pass
+	
 	
 func _physics_process(delta):
 	velocity.x += GRAVITY * delta * direction
 	if is_on_wall():
-		if Input.is_action_pressed("jump_left"):
+		jumps = 0
+		if Input.is_action_just_pressed("jump") and velocity.x > 0:
 			print("left")
 			$AnimatedSprite2D.flip_v = true
 			direction = -1
-		elif Input.is_action_pressed("jump_right"):
+		elif Input.is_action_just_pressed("jump") and velocity.x < 0:
 			print("right")
 			$AnimatedSprite2D.flip_v = false
 			direction = 1
-			
+	elif jumps == 0:
+		if Input.is_action_just_pressed("jump"):
+			jumps += 1
+			velocity.x = 0 
+			print("reverse_direction")
+			direction = direction * -1
+			if 	$AnimatedSprite2D.flip_v:
+				$AnimatedSprite2D.flip_v = false
+			else:
+				$AnimatedSprite2D.flip_v = true
+
+	
 	move_and_slide()
--- world.gd
+++ world.gd
@@ -1,26 +1,26 @@
 extends Node2D
 
-var palmtree_scene = preload("res://palmtree.tscn")
+var palmtree_scene = preload("res://Scenes/palmtree.tscn")
 var obstacle_types := [palmtree_scene]
 var obstacles : Array
 
-const BEACHBALL_START_POS := Vector2i(1114, 578)
-const CAM_START_POS := Vector2i(576, 324)
+const BEACHBALL_START_POS := Vector2i(1844, 850)
+const CAM_START_POS := Vector2i(960, 540)
 
 var score : float
 var speed : float
-var SCORE_MODIFIER : int = 10
-const START_SPEED : float = 2
-const MAX_SPEED : int = 25
+var SPEED_MODIFIER : int = 10
+const START_SPEED : float = 1
+const MAX_SPEED : int = 4
 var screen_size : Vector2i
 var last_obs
-
+var game_running : bool = false
+var obs_spawn_pos = [Vector2i(1844, -1080), Vector2i(50, -1080)]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_window().size
-
 	new_game()
 	
 func new_game():
@@ -30,31 +30,40 @@ func new_game():
 	$Beachball.position = BEACHBALL_START_POS
 	$Beachball.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POS
-	$Ground.position = Vector2i(0, 648)
+	$Ground.position = Vector2i(0, 1080)
+	$Camera2D/HUD.get_node("StartLabel").visible = true
 
-#func generate_obs():
-	#if obstacles.is_empty():
-		#var obs_type = obstacle_types[randi() % obstacle_types.size()]
-		#var obs
-		#obs = obs_type.instantiate()
-		#last_obs = obs
-		#add_child(obs)
-		#obstacles.append(obs)
+func generate_obs():
+	if obstacles.is_empty():
+		var obs_type = obstacle_types[randi() % obstacle_types.size()]
+		var obs
+		obs = obs_type.instantiate()
+		last_obs = obs
+		obs.position = obs_spawn_pos[randi() % obs_spawn_pos.size()]
+		add_child(obs)
+		obstacles.append(obs)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	speed = START_SPEED
-	#generate_obs()
-	#move character and camera
-	$Beachball.position.y -= speed
-	$Camera2D.position.y -= speed
+	if game_running:
+		speed = START_SPEED
+		generate_obs()
+		#move character and camera
+		$Beachball.position.y -= speed
+		$Camera2D.position.y -= speed
+		
+		$Camera2D/HUD.get_node("StartLabel").visible = false
+		score += speed
+
+		$Camera2D/HUD.get_node("ScoreLabel").text = "SCORE: " + str(score)
+
+		if -1*($Camera2D.position.y - $Ground.position.y) > screen_size.y * 1.5:
+			$Ground.position.y -= screen_size.y
+	else:
+		if Input.is_action_just_pressed("jump"):
+			$Beachball.get_node("AnimatedSprite2D").play("run")
+			game_running = true
 	
-	score += speed 
-	$Camera2D/Score.text = "SCORE: " + str(score)
-	
-	print($Camera2D.position.y - $Ground.position.y)
-	if -1*($Camera2D.position.y - $Ground.position.y) > screen_size.y * 1.5:
-		$Ground.position.y -= screen_size.y
 
 
 
